f1dc915477395fdfcc6fc363af03f7e9dd085ba9
Lucas Barbosa de Oliveira <lukasbo9765@gmail.com>
2025-03-06 17:03:39 -0300

    Customizando as opções admin.ModelAdmin (Área administrativa do Django

--- contact/admin.py
+++ contact/admin.py
@@ -8,4 +8,16 @@ from contact import models
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
     # criando classe que recebe decoretor(e recebe admin.ModelAdmin)
-    ...
+    list_display = 'id', 'first_name', 'last_name', 'phone',
+    # ordenando por
+    ordering = '-id',
+    # barra de pesquisa para os colunas determinadas
+    search_fields = 'id', 'first_name', 'last_name',
+    # 10 por página
+    list_per_page = 10
+    # max de 100 por página
+    list_max_show_all = 200
+    # opção do adm editar direto o argumetos da tupla
+    list_editable = 'first_name', 'last_name',
+    # colocando os argumentos como link de acesso ao dado
+    list_display_links = 'id', 'phone',
