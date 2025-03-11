4494823c80792a6438b1d968ca8a3ae08d6e25b5
Lucas Barbosa de Oliveira <lukasbo9765@gmail.com>
2025-03-11 16:43:32 -0300

    Criando arquivos parciais para usar com include nos templates

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, get_object_or_404
 from contact.models import Contact
-from django.http import Http404
 
 # view que pega a requisição index e retorna a renderização como resposta determinado html
 def index(request):
@@ -11,13 +10,15 @@ def index(request):
     context = {
         # esse contact pode ser iterado -> é um objeto com vários objetos
         'contacts': contacts,
+        # passando no contexto um valor para site_title
+        'site_title': 'Contatos - '
     }
 
     return render(
         request,
         'contact/index.html',
         # index retorna todos os dados de Contact para ser manipulado
-        context
+        context,
     )
 
 # criando a view contact, que recebe a "requisição" com o "contato_id" passado pela urls como parâmetro
@@ -28,9 +29,14 @@ def contact(request, contact_id):
     single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
     # Pegando da tabela Contact, a pk que seja = contact_id, e que esteja com show ok
 
+    # criando uma variável para atribuir os valores para a chave site_title
+    contact_name = f'{single_contact.first_name} {single_contact.last_name} - '
+
     # context armazena os dados em contact para serem repassados para o template
     context = {
         'contact': single_contact,
+        # passando no contexto um valor para site_title
+        'site_title': contact_name
     }
 
     # retorna a requisição, joga para o template desejado, e com o contexto para uso
