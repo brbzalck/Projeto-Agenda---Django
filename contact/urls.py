d484a7a0b4e704116f454dbcbdff868e16221416
Lucas Barbosa de Oliveira <lukasbo9765@gmail.com>
2025-03-17 15:40:37 -0300

    Usando um padrão comum para URLs de CRUD

--- contact/urls.py
+++ contact/urls.py
@@ -6,10 +6,10 @@ app_name = 'contact'
 
 # onde fica mapeado as url
 urlpatterns = [
-    # url manda o contact_id como parâmetro, chama a view contact, com nome de url contact
-    path('<int:contact_id>/', views.contact, name='contact'),
-    # url requisitada pelo form, que puxa view search, de nome search
-    path('search/', views.search, name='search'),
     # 'index do site', execução da função, nomeando a URL
     path('', views.index, name='index'),
+    # url requisitada pelo form, que puxa view search, de nome search
+    path('search/', views.search, name='search'),
+    # url de endereço manda o contact_id como parâmetro, chama a view contact, com nome de url contact
+    path('contact/<int:contact_id>/', views.contact, name='contact'),
 ]
