9d9ac1e321b80f4e2e5682f949910317e5498b57
Lucas Barbosa de Oliveira <lukasbo9765@gmail.com>
2025-03-18 17:51:05 -0300

    Configurando os campos e widgets do formulário

--- contact/forms.py
+++ contact/forms.py
@@ -4,6 +4,27 @@ from . import models
 
 # Criando form baseado no Model django
 class ContactForm(forms.ModelForm):
+    # editando a column first_name da model com widget para atributos na label
+    first_name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'placeholder': 'Escreva aqui'
+            }
+        ),
+        label='Primeiro Nome',
+        help_text='Texto de ajuda',
+    )
+
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # pegando pelo init do contactForm o first_name e modificando com widgets
+
+        # self.fields['first_name'].widget.attrs.update({
+        #     'placeholder': 'Escreva aqui!'
+        # })
+
     class Meta:
         # definindo qual model se trata
         model = models.Contact
@@ -11,6 +32,13 @@ class ContactForm(forms.ModelForm):
         fields = (
             'first_name', 'last_name', 'phone',
         )
+        # widgets = {
+        #     'first_name': forms.TextInput(
+        #         attrs={
+        #             'placeholder': 'Escreva aqui'
+        #         }
+        #     )
+        # }
 
     # função clean que pega os dados limpos do POST
     def clean(self):
