071f687b6c7913874fd06b63712070825ab02bcc
Lucas Barbosa de Oliveira <lukasbo9765@gmail.com>
2025-03-18 16:23:29 -0300

    Criando um formulário dinâmico com forms.ModelForm do Django (parte 1)

--- contact/views/contact_form.py
+++ contact/views/contact_form.py
@@ -1,28 +1,44 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.db.models import Q
 from django.core.paginator import Paginator
+from django import forms
 
 from contact.models import Contact
 
+# Criando form baseado no Model django
+class ContactForm(forms.ModelForm):
+    class Meta:
+        # definindo qual model se trata
+        model = Contact
+        # selecioanando as columns ta table
+        fields = (
+            'first_name', 'last_name', 'phone',
+        )
 
+
+# função de requisição que renderiza a criação de dados
 def create(request):
-    # se o método for post executa isso
+    # se o usuário entrar com dados == POST
     if request.method == 'POST':
-        print()
-        print(request.method)
-        print(request.POST.get('first_name'))
-        print(request.POST.get('last_name'))
-        print()
+        # enviando para o context os dados obtidos de POST
+        context = {
+            'form': ContactForm(request.POST)
+        }
 
+        # se action form = True, renderiza novamente o create com os dados preenchidos
+        return render(
+        request,
+        'contact/create.html',
+        context,
+    )
+
+    # se não existir entrada de dados, apenas acesso ao create
     context = {
-
+        # envia os campos em branco para preenchimento
+        'form': ContactForm()
     }
 
-    print()
-    # verificando método caso não tenha nenhum input no Send(post)
-    print(request.method)
-    print()
-
+    # renderiza a requisição com a resposta create vazia para preenchimento
     return render(
         request,
         'contact/create.html',
