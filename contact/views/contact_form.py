ccf324a5d4cc69bfb8639bc12bd7bf4a509e25d3
Lucas Barbosa de Oliveira <lukasbo9765@gmail.com>
2025-03-19 14:29:17 -0300

    Verificando form.is_valid, salvando com form.save e redirecionando a página

--- contact/views/contact_form.py
+++ contact/views/contact_form.py
@@ -1,14 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from contact.forms import ContactForm
 
 # função de requisição que renderiza a criação de dados
 def create(request):
     # se o usuário entrar com dados == POST
     if request.method == 'POST':
+        # salvando os dados obtidos pelo POST
+        form = ContactForm(request.POST)
         # enviando para o context os dados obtidos de POST
         context = {
-            'form': ContactForm(request.POST)
+            'form': form
         }
+        # se o formulário estiver válido
+        if form.is_valid():
+            # salva na base de dados
+            form.save()
+            # e redireciona para a view create
+            return redirect('contact:create')
+
 
         # se action form = True, renderiza novamente o create com os dados preenchidos
         return render(
