701b7cbd0fda809c6e60a5508ea1c3208ed5eb39
Lucas Barbosa de Oliveira <lukasbo9765@gmail.com>
2025-03-19 14:10:28 -0300

    Validando campos do formulário com clean, clean_field e ValidationError

--- contact/forms.py
+++ contact/forms.py
@@ -40,20 +40,40 @@ class ContactForm(forms.ModelForm):
         #     )
         # }
 
-    # função clean que pega os dados limpos do POST
+    # função clean que pega os dados limpos do POST(tem acesso a tds campos do form)
     def clean(self):
+        # usando cleaned_data no objeto para acessar os campos do form
         cleaned_data = self.cleaned_data
+        # pegando os valores de entrada nos campos first e last name
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
 
-        # adicionando um erro propositalmente
-        self.add_error(
+        if first_name == last_name:
+            # criando msg de erro com ValidationError para add.error
+            msg = ValidationError(
+                    'Primeiro nome não pode ser igual ao segundo',
+                    code='invalid',
+                )
+            # adicionando erro ao campo first_name, com determinada mensagem
+            self.add_error('first_name', msg)
+            self.add_error('last_name', msg)
+
+        # método clean retorna super().clean() para fazer quantas validações forem precisas
+        return super().clean()
+
+    # função clean para campo específico que retorna o próprio campo
+    def clean_first_name(self):
+        # pegando os dados limpos do POST onde o campo for = first_name
+        first_name = self.cleaned_data.get('first_name')
+
+        if first_name == 'ABC':
+            # adicionando um erro no objeto
+            self.add_error(
             # em first_name
             'first_name',
             # com essa mensagem e código de erro
-            ValidationError('Mensagem Erro', code='invalid')
-        )
-        self.add_error(
-            'first_name',
             ValidationError('Mensagem Erro 2', code='invalid')
         )
 
-        return super().clean()
+        # método epecifico de clean_campo retorna o próprio campo para manter no form como exemplo
+        return first_name
